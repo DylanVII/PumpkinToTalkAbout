847b1431a3ffa367630248408f034e9d6ae76ef5
JonahGM <59736650+JonahGM@users.noreply.github.com>
2020-01-24 18:50:44 -0500

    Added to PlayerMovement and Created PlayerController
    Added functions to PlayerMovement as well as created PlayerController script.

--- PumpkinToTalkAbout/Assets/Scripts/Movement.cs
+++ PumpkinToTalkAbout/Assets/Scripts/Movement.cs
@@ -16,6 +16,9 @@ public class Movement : MonoBehaviour
     private float accelTime;
     [SerializeField]
     private float decelTime;
+
+
+    public int playerNum;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,8 +26,57 @@ public class Movement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void PlayerMovementUpdate()
     {
-        
+        //General Movement (Keyboard)
+        input.x = Input.GetAxisRaw("HorizontalK_P" + playerNum);
+        input.y = Input.GetAxisRaw("VerticalK_P" + playerNum);
+
+
+
+
+
+
+        if (input.x != 0)
+        {
+            lastRecordedInput.x = input.x;
+        }
+        if (input.y != 0)
+        {
+            lastRecordedInput.y = input.y;
+        }
+
+
     }
+
+
+    public void PlayerMovementFixedUpdate()
+    {
+        float acceleration = maxSpeed / accelTime * Time.fixedDeltaTime;
+        float deceleration = maxSpeed / decelTime * Time.fixedDeltaTime;
+
+        if (input.x != 0)
+        {
+            inputSpeed.x = Mathf.Clamp(inputSpeed.x + acceleration, 0, 1);
+        }
+        else
+        {
+            inputSpeed.x = Mathf.Clamp(inputSpeed.x - deceleration, 0, 1);
+        }
+        if (input.y != 0)
+        {
+            inputSpeed.y = Mathf.Clamp(inputSpeed.y + acceleration, 0, 1);
+        }
+        else
+        {
+            inputSpeed.y = Mathf.Clamp(inputSpeed.y - deceleration, 0, 1);
+        }
+
+        Vector2 velocity = new Vector2(lastRecordedInput.x * inputSpeed.x, lastRecordedInput.y * inputSpeed.y);
+
+        rigBod.velocity = velocity.normalized * velocity.magnitude * maxSpeed;
+    }
+
+
+
 }
