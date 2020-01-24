4e612c99f23120d5ce1862dd8d118e566542559d
Adam Wilczewski <trollax88@gmail.com>
2020-01-24 11:58:30 -0500

    Added base Gamemanager
    Added gamemanager that will track the time, score and the state of the game. This is for the setting up of the scene.

---
+++ PumpkinToTalkAbout/Assets/Scripts/Game_Manager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Manager : MonoBehaviour
+{
+    public float gameTime;
+    public int score;
+
+    public enum State { Starting, Playing,Gameover};
+    public State state;
+
+
+    void Start()
+    {
+        state = State.Starting;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case (State.Starting):
+
+                break;
+            case (State.Playing):
+
+                break;
+            case (State.Gameover):
+
+                break;
+
+        }
+    }
+}
