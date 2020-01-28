58106aa9d07c8c02e36c28f0c0b1277429e3155d
King_Ram <trollax88@gmail.com>
2020-01-27 21:45:43 -0500

    Added Tittle Screen Functionality
    Added Start and quit functionality

---
+++ PumpkinToTalkAbout/Assets/InitialFolder/Scripts/Tittle_Scripts/Scene_Manager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Manager : MonoBehaviour
+{
+    public string gameScene;
+
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+
+}
