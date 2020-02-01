4ef458096b626a527825e97353e900eafaad1f6f
TED CRUZUMAKI <SasquatchOverload@gmail.com>
2020-02-01 15:26:23 -0500

    Edited Script For Merge Conflict
    Edited 'Javelin.cs' and attached to Test Farmer in Main_Game Scene.

--- PumpkinToTalkAbout/Assets/InitialFolder/Scripts/Javelin Related Scripts/Javelin.cs
+++ PumpkinToTalkAbout/Assets/InitialFolder/Scripts/Javelin Related Scripts/Javelin.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Javelin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float throwStrength = 10f;
+    
+    public void ThrowJavelin ()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
