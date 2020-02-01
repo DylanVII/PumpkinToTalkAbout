fb2a8b5a30dd51269564038b06296af0f0d3bf1e
TED CRUZUMAKI <SasquatchOverload@gmail.com>
2020-02-01 18:40:18 -0500

    Updated Ghost.cs
    Added tentative status states to Ghost.cs to determine if the player ghost is stunned or not. Most likely may switch to a bool check if there is no need to expand on additional statuses.

--- PumpkinToTalkAbout/Assets/InitialFolder/Scripts/Javelin Related Scripts/Ghost.cs
+++ PumpkinToTalkAbout/Assets/InitialFolder/Scripts/Javelin Related Scripts/Ghost.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Ghost : MonoBehaviour
 {
+    public _states status;
+    public enum _states
+    {
+        normal,
+        stunned
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,15 @@ public class Ghost : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        switch (status)
+        {
+            case _states.normal:
+                //be ok
+                break;
+
+            case _states.stunned:
+                //be not ok
+                break;
+        }
     }
 }
