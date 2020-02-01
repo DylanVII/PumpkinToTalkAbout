906ce715472eb8af7a7dccbffad9f944ed5235ff
TED CRUZUMAKI <SasquatchOverload@gmail.com>
2020-02-01 17:51:12 -0500

    Updated Javelin.cs
    Added basic functionality when the Javelin collides with a Pumpkin (return to player after collision).

--- PumpkinToTalkAbout/Assets/InitialFolder/Scripts/Javelin Related Scripts/Javelin.cs
+++ PumpkinToTalkAbout/Assets/InitialFolder/Scripts/Javelin Related Scripts/Javelin.cs
@@ -7,26 +7,64 @@ using UnityEngine;
  * Script that sets the functionality behind the "javelin".
  * Apply onto the Javelin prefab.
  * 
+ * Not really too satisfied with how the code functions (probably forgetting
+ * some obvious fundamentals cuz smol brain), but will have to do for now.
  */
 public class Javelin : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject parent;
+
     private new Collider collider;
+
+    private bool isReeling = false;
+
+
+    private float reelSpeed = 5f; //Speed for when the Javelin returns to the player.
+
     void Start()
     {
         if (!collider)
             collider = GetComponent<Collider>();
     }
 
+
+
+    void Update()
+    {
+
+        if (isReeling)
+            PullObjectToLocation(parent.transform.position, reelSpeed);
+    }
+
+
+
+    public void PullObjectToLocation(Vector3 endPosition, float reelingSpeed)
+    {
+        if (transform.position != endPosition)
+        {
+            float step = reelingSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, endPosition, step);
+        }
+        else
+        {
+            isReeling = false;
+        }
+    }
+
     public void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Pumpkin")
         {
-
+            isReeling = true;
         }
 
         if (other.gameObject.tag == "Ghost")
         {
+            Debug.Log("Impaled a ghost boi");
 
+            //Do stun code
         }
     }
+
 }
