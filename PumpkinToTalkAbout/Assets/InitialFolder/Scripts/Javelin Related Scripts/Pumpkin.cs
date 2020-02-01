6b4c3c1487c266ce2d55784dc4983fe4cbb6908d
TED CRUZUMAKI <SasquatchOverload@gmail.com>
2020-02-01 18:26:23 -0500

    Updated Pumpkin.cs
    Pumpkins no longer collide while being pulled by a pitchfork.

--- PumpkinToTalkAbout/Assets/InitialFolder/Scripts/Javelin Related Scripts/Pumpkin.cs
+++ PumpkinToTalkAbout/Assets/InitialFolder/Scripts/Javelin Related Scripts/Pumpkin.cs
@@ -6,15 +6,20 @@ public class Pumpkin : MonoBehaviour
 {
     public bool isGrabbed = false;
 
+    private new Collider collider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        collider = GetComponent<Collider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isGrabbed)
+            collider.enabled = false;
+        else
+            collider.enabled = true;
     }
 }
