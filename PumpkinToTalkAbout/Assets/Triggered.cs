44913c367bb5ab1e54f50f4d9253e3ba8c1c9054
Adam Wilczewski <trollax88@gmail.com>
2020-02-21 11:44:02 -0500

    Made working grab
    grab works when pressing b near a pumkin. There is commented code to allow the implementation of rotation to the ghosts.

--- PumpkinToTalkAbout/Assets/Triggered.cs
+++ PumpkinToTalkAbout/Assets/Triggered.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class Triggered : MonoBehaviour
 {
     public bool triggered = false;
+    public GameObject targetPumpkin;
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Pumpkin")
         {
+            targetPumpkin = other.gameObject; 
             triggered = true;
         }
-        else
-        {
-            triggered = false; 
+       
+    }
 
-        }
+    private void OnTriggerExit(Collider other)
+    {
+        triggered = false;
     }
 }
