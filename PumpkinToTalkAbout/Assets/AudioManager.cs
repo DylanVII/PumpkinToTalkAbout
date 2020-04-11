ac685f7f1fe7dcc8960c56467241858fb294dc9c
crimsonauts <56197888+crimsonauts@users.noreply.github.com>
2020-04-10 20:32:45 -0400

    Pause Menu
    Created a pause menu for UI

---
+++ PumpkinToTalkAbout/Assets/AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    AudioSource s;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        s = GetComponent<AudioSource>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PauseMenu.GameIsPaused)
+        {
+            s.pitch *= .9f;
+        } else if (!PauseMenu.GameIsPaused)
+        {
+            s.pitch = 1f;
+        }
+
+        if (PauseMenu.BackToMenu)
+        {
+            s.pitch = 1f;
+        }
+    }
+}
---
+++ PumpkinToTalkAbout/Assets/InitialFolder/Scripts/UI Related/PauseMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public static bool BackToMenu = false;
+
+    public GameObject pauseMenuUI;
+
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        BackToMenu = false;
+    }
+
+    public void BackButton()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Tittle_Screen");
+        BackToMenu = true;
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
